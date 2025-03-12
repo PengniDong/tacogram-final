7fe9ecbe3f7c13f5f3b3fb0c4c4d4941e9a6edb9
Pengni Dong <pengni.dong@kellogg.northwestern.edu>
2025-03-12 23:36:56 +0000

    a

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,14 @@
 class PostsController < ApplicationController
-
   def index
     @posts = Post.all
 
     # alternative responses to requests other than HTML
-    # respond_to do |format|
-    #   format.html # implicitly renders posts/index.html.erb
-    # end
+    respond_to do |format|
+      format.html # implicitly renders posts/index.html.erb
+      format.json do
+        render :json => @posts
+      end
+    end
   end
 
   def new
@@ -18,7 +20,6 @@ class PostsController < ApplicationController
     if @user != nil
       @post = Post.new
       @post["body"] = params["body"]
-      @post["image"] = params["image"]
       @post.uploaded_image.attach(params["uploaded_image"])
       @post["user_id"] = @user["id"]
       @post.save
@@ -28,12 +29,12 @@ class PostsController < ApplicationController
     redirect_to "/posts"
   end
 
-  # before_action :allow_cors
-  # def allow_cors
-  #   response.headers['Access-Control-Allow-Origin'] = '*'
-  #   response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
-  #   response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
-  #   response.headers['Access-Control-Max-Age'] = '1728000'
-  # end
-
+  # removes security restrictions for API calls
+  before_action :allow_cors
+  def allow_cors
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
+    response.headers['Access-Control-Max-Age'] = '1728000'
+  end
 end
